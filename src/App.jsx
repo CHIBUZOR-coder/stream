d5f7de7cd528a13d14f46d0c5de852eed9b1027b
Chibuzor <chibuzormekalam@gmail.com>
2024-12-18 01:39:35 +0100

    added some needed pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import { Outlet } from "react-router-dom";
+ import "swiper/css";
+ import "swiper/css/pagination"
+ import "swiper/css/navigation"
+ import "aos"
+ import "aos/dist/aos.css"
+
 
 function App() {
+ 
   return (
     <>
       <Outlet />
