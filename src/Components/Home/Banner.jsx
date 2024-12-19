6c03d518a91ec099fa2e602f339dd8b54da74302
Chibuzor <chibuzormekalam@gmail.com>
2024-12-19 12:26:03 +0100

    added banner to home and vertical slider in banner

--- src/Components/Home/Banner.jsx
+++ src/Components/Home/Banner.jsx
@@ -1,22 +1,41 @@
 import { Autoplay } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Movies } from "../../Data/MovieData";
 
 const Banner = () => {
+  const movieList = Movies;
+  console.log(movieList);
+
   return (
-    <div>
+    <div className="relative w-full">
+      {/* <div
+        className={`bg-[url('/image/perfect.jpg')] bg-cover bg-center relative overflow-hidden rounded  p-2`}
+      >
+        
+        lorem20
+      </div> */}
+
       <Swiper
-      direction="vertical"
+        direction="vertical"
         spaceBetween={0}
         slidesPerView={1}
         loop={true}
         speed={1000}
         modules={[Autoplay]}
         autoplay={{ delay: 5000, disableOnInteraction: false }}
-        className="w-full xl:h-96 bg-dry lg:h-64 h-48"
+        className="w-full h-72  lg:h-72 xl:h-96 bg-dry"
       >
-
-
-
+        {movieList.slice(0, 8).map((movie) => (
+          <SwiperSlide
+            key={movie.id}
+            className="bg-center bg-cover relative overflow-hidden rounded"
+            style={{
+              backgroundImage: `url('./images/${movie.image}.jpg')`,
+            }}
+          >
+            {movie.image}
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   );
--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -58,12 +58,12 @@ const Footer = () => {
   return (
     <div className="bg-dry py-4  px-4 border-2 border-black w-full">
       <div className="flex  flex-wrap gap-4 md:gap-10  items-center w-full justify-between md:justify-center">
-        {Links.map((link) => (
-          <div className="flex flex-col justify-start  items-start my-2 md:my-0 footText gap-5 md:gap-6 w-[40%] md:w-[20%] ">
+        {Links.map((link, index) => (
+          <div key={index} className="flex flex-col justify-start  items-start my-2 md:my-0 footText gap-5 md:gap-6 w-[40%] md:w-[20%] ">
             <p className=" font-semibold">{link.title}</p>
             <div className="flex flex-col gap-4 justify-start items-start">
-              {link.links.map((item) => (
-                <p className="text-border transi hover:text-subMain cursor-pointer  ">
+              {link.links.map((item, index) => (
+                <p key={index} className="text-border transi hover:text-subMain cursor-pointer  ">
                   {item.title}
                 </p>
               ))}
