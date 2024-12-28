863f346eacba2dbdecc6f3af8728ed7a025c8c7d
CHIBUZOR-coder <CHIBUZOR-coder@users.noreply.github.com>
2024-12-28 12:40:25 +0000

    deploy: 7979afe81e6ed11ad90d0b84b6d72f1282ce0ddd

---
+++ assets/FavouriteMovies-EKPJxdDA.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-B7RBun8j.js";import{L as t}from"./Layout-7Z2qzrPP.js";const i=()=>r.jsx(t,{children:r.jsx("div",{children:"Favourite"})});export{i as default};
--- assets/NotFound-DipsOevA.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-3yOwvcFn.js";const n=()=>o.jsx("div",{children:o.jsx("p",{children:"Page  not found"})});export{n as default};
