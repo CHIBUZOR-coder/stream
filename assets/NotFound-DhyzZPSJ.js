cf9774c9f3d12206286e4a3d0d859867d5474c4e
CHIBUZOR-coder <CHIBUZOR-coder@users.noreply.github.com>
2024-12-30 16:30:00 +0000

    deploy: 40bea02f1f6c50b4f28360fd9a7fc46c41d28c33

---
+++ assets/NotFound-DhyzZPSJ.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-DN-W4z8Q.js";const n=()=>o.jsx("div",{children:o.jsx("p",{children:"Page  not found"})});export{n as default};
--- assets/Users-BHh4E6SO.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-D6SewQm7.js";import{L as s}from"./Layout-CQo-OX3u.js";const o=()=>r.jsx(s,{children:r.jsx("div",{children:"User"})});export{o as default};
