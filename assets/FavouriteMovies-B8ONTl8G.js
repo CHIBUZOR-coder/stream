342a00255be26006c9b95d01f8aa90592fabdf5f
CHIBUZOR-coder <CHIBUZOR-coder@users.noreply.github.com>
2025-01-03 16:37:56 +0000

    deploy: 25ef88312f0bce6d9ed4f784a5eebe01a52c5c4d

--- assets/FavouriteMovies-B8ONTl8G.js
+++
@@ -1,1 +0,0 @@
-import{j as r,g as t}from"./index---6laPj0.js";const o=()=>r.jsx(t,{children:r.jsx("div",{children:"Favourite"})});export{o as default};
---
+++ assets/Users-K59_qcJp.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-BeVra00e.js";import{L as s}from"./Layout-BCYDH0uP.js";const o=()=>r.jsx(s,{children:r.jsx("div",{children:"User"})});export{o as default};
