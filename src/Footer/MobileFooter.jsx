aa93951a8d68d30b496dff5b829841a01a1dd421
Chibuzor <chibuzormekalam@gmail.com>
2025-01-09 10:24:16 +0100

    fixed navigation issue

--- src/Footer/MobileFooter.jsx
+++ src/Footer/MobileFooter.jsx
@@ -1,57 +1,16 @@
 import { useEffect, useState } from "react";
 import { BsCollectionPlay, BsCollectionPlayFill } from "react-icons/bs";
-import { CgMenu } from "react-icons/cg";
+import { LuSquareMenu } from "react-icons/lu";
 import { FaUserCircle } from "react-icons/fa";
 import { FaHeartCircleCheck } from "react-icons/fa6";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { BiHomeAlt } from "react-icons/bi";
 
 const MobileFooter = () => {
-  const [isActive, setIsActive] = useState(() => {
-    return localStorage.getItem("MobileActive") || "home";
-  });
-const HandleActive = (value) => {
-  if (value === "") {
-    return; // Do nothing if the value is empty
-  }
-  localStorage.setItem("MobileActive", value); // Update localStorage
-  setIsActive(value); // Directly update the state with the new value
-};
+  const hover = "hover:text-subMain transi text-white relative";
+  const Hover = ({ isActive }) => (isActive ? "text-subMain  " : hover);
 
 
-  useEffect(() => {
-    console.log(isActive);
-  }, [isActive]);
-  const MobileLinks = [
-    {
-      icon: <BiHomeAlt className="w-6 h-6" />,
-      path: "/stream/",
-      name: "home",
-      bg: false,
-    },
-    {
-      icon: <FaHeartCircleCheck className="w-6 h-6" />,
-      path: "/stream/favouritpage",
-      name: "favourite",
-      bg: false,
-    },
-    {
-      icon: <FaUserCircle className="w-6 h-6" />,
-      path: "/stream/login",
-      name: "login",
-      bg: false,
-    },
-    {
-      icon: (
-        <button>
-          <CgMenu className="w-6 h-6" />
-        </button>
-      ),
-      path: "",
-      name: "",
-      bg: true,
-    },
-  ];
 
   return (
     <>
@@ -60,22 +19,50 @@ const HandleActive = (value) => {
         <div className="fixed lg:hidden z-50 bottom-0 w-full">
           <div className="bg-main flex justify-between items-center w-full p-2 gap-4">
             <div className="flex w-full justify-between">
-              {MobileLinks.map((mobLink, i) => (
-                <Link
-                  key={i}
-                  to={mobLink.path}
-                  onClick={() => HandleActive(mobLink.name)}
-                  className={`${
-                    isActive === mobLink.name
-                      ? "bg-white text-main"
-                      : "text-white"
-                  } py-2 px-3 rounded-md flex justify-center items-center hover:text-main hover:bg-white  ${
-                    mobLink.bg ? "text-white " : ""
-                  }  `}
-                >
-                  {mobLink.icon}
-                </Link>
-              ))}
+              <NavLink
+                className={({ isActive }) =>
+                  `${
+                    isActive
+                      ? "text-main bg-white"
+                      : "hover:text-main hover:bg-white transi text-white relative"
+                  } hov  p-[0.9rem]  flex justify-center items-center rounded-md`
+                }
+                to={`/stream/movies`}
+              >
+                <BsCollectionPlayFill className="w-6 h-6" />
+              </NavLink>
+
+              <NavLink
+                className={({ isActive }) =>
+                  `${
+                    isActive
+                      ? "text-main bg-white"
+                      : "hover:text-main hover:bg-white transi text-white relative"
+                  } hov mobie_notiP   p-[0.9rem]  flex justify-center items-center rounded-md`
+                }
+                to={`/stream/favouritpage`}
+              >
+                <FaHeartCircleCheck className="w-6 h-6" />
+                <p className="w-4 h-4 flexCol_mdRow rounded-full hova mobie_noti text-xs bg-subMain text-white absolute top-[1px] right-[10px]">
+                  3
+                </p>
+              </NavLink>
+              <NavLink
+                className={({ isActive }) =>
+                  `${
+                    isActive
+                      ? "text-main bg-white"
+                      : "hover:text-main hover:bg-white transi text-white relative"
+                  } hov  p-[0.9rem] flex justify-center items-center rounded-md`
+                }
+                to={`/stream/login`}
+              >
+                <FaUserCircle className="w-6 h-6" />
+              </NavLink>
+
+              <button className=" px-4 py-3 flex justify-center items-center rounded-md hover:bg-white  hover:text-main transi">
+                <LuSquareMenu className="w-6 h-6" />
+              </button>
             </div>
           </div>
         </div>
