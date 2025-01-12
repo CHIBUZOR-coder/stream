53caf62e1f2a94d49058266eec7a8f382de57c90
CHIBUZOR-coder <CHIBUZOR-coder@users.noreply.github.com>
2025-01-12 07:21:35 +0000

    deploy: d040858f4247f35df292a7e7f840e9751efd8eb8

--- assets/SelectRating-oDJTl0-w.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-DP6Wmuw3.js";const o=({data:r,setRating:a})=>t.jsx("select",{className:"w-full mt-2 px-3 py-4 text-text bg-main border border-border rounded appearance-none",id:"",onChange:e=>a(e.target.value),children:r.map((e,n)=>t.jsx("option",{value:e.value,children:e.tittle},n))});export{o as S};
