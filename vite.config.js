71fd56eeebf92e7f5269021c812c1222dd83eb96
Chibuzor <chibuzormekalam@gmail.com>
2025-02-17 08:51:33 +0100

    fxing router to allw varcel rendering3

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base:"/stream/",
+  
+// })
+
+
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-  base:"/stream/",
-  
-})
-
-
-
+  base: "/", // Use "/" for Vercel
+});
