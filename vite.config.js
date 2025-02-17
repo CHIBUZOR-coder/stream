585921b443796d3ae1a881b23a39a70984e91ae4
Chibuzor <chibuzormekalam@gmail.com>
2025-02-17 08:30:26 +0100

    fxing router to allw varcel rendering

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base:"/stream/",
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base:"/stream/",
   
-})
+// })
 
 
-// import { defineConfig, loadEnv } from "vite";
-// import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-// export default defineConfig(({ mode }) => {
-//   // Load the correct environment file
-//   const env = loadEnv(mode, process.cwd(), "VITE");
+export default defineConfig(({ mode }) => {
+  // Load the correct environment file
+  const env = loadEnv(mode, process.cwd(), "VITE");
 
-//   return {
-//     plugins: [react()],
-//     base: env.VITE_BASE_URL || "/", // Use the correct base path
-//   };
-// });
+  return {
+    plugins: [react()],
+    base: env.VITE_BASE_URL || "/", // Use the correct base path
+  };
+});
