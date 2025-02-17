4594aec455efb1ce008dc885a721e780d00b5608
Chibuzor <chibuzormekalam@gmail.com>
2025-02-17 08:40:50 +0100

    fxing router to allw varcel rendering

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,12 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base:"/stream/",
-  
-// })
-
-
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { viteStaticCopy } from "vite-plugin-static-copy";
-
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [{ src: "404.html", dest: "" }],
-    }),
-  ],
-  base: "/stream/", // Change this if deploying to GitHub Pages
-});
+  plugins: [react()],
+  base:"/stream/",
+  
+})
+
+
+
