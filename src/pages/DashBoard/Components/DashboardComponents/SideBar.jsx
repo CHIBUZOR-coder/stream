d2e4aad9b787e75b02a435f11396bbfb43882fde
Chibuzor <chibuzormekalam@gmail.com>
2025-02-18 12:44:51 +0100

    fiexd logout redirect nav

--- src/pages/DashBoard/Components/DashboardComponents/SideBar.jsx
+++ src/pages/DashBoard/Components/DashboardComponents/SideBar.jsx
@@ -32,7 +32,7 @@ const SideBar = () => {
 
       if (res.ok) {
         // Make sure to wait for the response
-        nabvigate("/stream/login");
+        nabvigate("/login");
         console.log(data);
       } else {
         console.log("Failed to clear cookies. Server returned an error.");
