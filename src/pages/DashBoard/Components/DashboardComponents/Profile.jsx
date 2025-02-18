019a897eff79fb5a1bf86cf6114ba4373dbd21f2
Chibuzor <chibuzormekalam@gmail.com>
2025-02-18 12:12:25 +0100

    Changed url in profile

--- src/pages/DashBoard/Components/DashboardComponents/Profile.jsx
+++ src/pages/DashBoard/Components/DashboardComponents/Profile.jsx
@@ -46,7 +46,7 @@ const Profile = ({ Handlegeneral, HandleDeleteMovie, setModalDisplay }) => {
     // console.log("userData.role:", userData.role);
 
     if (userData.role === "ADMIN") {
-      UserLink = `https://streambackend-ngow.onrender.com.com/getAdmin/${name}`;
+      UserLink = `https://streambackend-ngow.onrender.comgetAdmin/${name}`;
     } else if (userData.role === "USER") {
       UserLink = `https://streambackend-ngow.onrender.com/getUser/${name}`;
     } else {
