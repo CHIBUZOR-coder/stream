61e852c9ff3b39840b184bedfdef062d1bf8418f
Chibuzor <chibuzormekalam@gmail.com>
2025-02-19 10:16:30 +0100

    fix slide

--- src/Components/SingleMovie/MovieCasts.jsx
+++ src/Components/SingleMovie/MovieCasts.jsx
@@ -37,51 +37,55 @@ const MovieCasts = ({ movieId, movie }) => {
       </div>
 
       <div className="mt-10">
-        <Swiper
-          navigation={{ nextEl, prevEl }}
-          slidesPerView={4}
-          spaceBetween={40}
-          autoplay={true}
-          speed={1000}
-          modules={[Autoplay, Navigation]}
-          loop={true}
-          breakpoints={{
-            0: {
-              slidesPerView: 1,
-            },
-            400: {
-              slidesPerView: 2,
-            },
-            768: {
-              slidesPerView: 3,
-            },
-            1024: {
-              slidesPerView: 4,
-            },
-            1280: {
-              slidesPerView: 5,
-              spaceBetween: 30,
-            },
-          }}
-        >
-          {casts &&
-            casts.map((item) => (
-              <SwiperSlide key={item?.cast?.id} className="w-full p-4 ">
-                <div className="flexCol gap-2  italic w-[100%] text-xs text-text rounded bg-dry border pb-2 ">
-                  <div className="h-[300px] md:h-[250px] border-gray-800 w-full  bg-cover bg-center">
-                    <img
-                      className="h-full w-full object-cover"
-                      src={`${item?.cast?.image}`}
-                      alt=""
-                    />
-                  </div>
+        {casts.length > 0 ? (
+          <Swiper
+            navigation={{ nextEl, prevEl }}
+            slidesPerView={4}
+            spaceBetween={40}
+            autoplay={true}
+            speed={1000}
+            modules={[Autoplay, Navigation]}
+            loop={true}
+            breakpoints={{
+              0: {
+                slidesPerView: 1,
+              },
+              400: {
+                slidesPerView: 2,
+              },
+              768: {
+                slidesPerView: 3,
+              },
+              1024: {
+                slidesPerView: 4,
+              },
+              1280: {
+                slidesPerView: 5,
+                spaceBetween: 30,
+              },
+            }}
+          >
+            {casts &&
+              casts.map((item) => (
+                <SwiperSlide key={item?.cast?.id} className="w-full p-4 ">
+                  <div className="flexCol gap-2  italic w-[100%] text-xs text-text rounded bg-dry border pb-2 ">
+                    <div className="h-[300px] md:h-[250px] border-gray-800 w-full  bg-cover bg-center">
+                      <img
+                        className="h-full w-full object-cover"
+                        src={`${item?.cast?.image}`}
+                        alt=""
+                      />
+                    </div>
 
-                  <p>{item?.name}</p>
-                  <p>Role : {item?.cast?.role}</p>
-                </div>
-              </SwiperSlide>
-            ))}
-        </Swiper>
+                    <p>{item?.name}</p>
+                    <p>Role : {item?.cast?.role}</p>
+                  </div>
+                </SwiperSlide>
+              ))}
+          </Swiper>
+        ) : (
+          ""
+        )}
 
         <div className="w-full px-1 flex gap-6 pt-12 justify-center">
           <button className={className} ref={(node) => setPrev(node)}>
