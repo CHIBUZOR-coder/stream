d5d977cd1350ff3f71650c9ee5cd7249b67b5fa3
Chibuzor <chibuzormekalam@gmail.com>
2025-02-21 11:07:40 +0100

    reduced height when no user

--- src/Components/SingleMovie/MovieRates.jsx
+++ src/Components/SingleMovie/MovieRates.jsx
@@ -152,7 +152,7 @@ const MovieRates = ({ movie }) => {
         </div>
 
         {/* Review */}
-        <div className="col-span-3 flex flex-col gap-6">
+        <div className="col-span-3 flex flex-col gap-6 w-full md:w-auto">
           <p className="text-xl text-text font-semibold">Reviews (56)</p>
           <div className={`w-full flex flex-col bg-main gap-6 rounded-lg md:p-12 p-0   h-[300px] md:h-[550px] overflow-y-scroll `}>
             {starUsers && starUsers.length > 0 ? (
