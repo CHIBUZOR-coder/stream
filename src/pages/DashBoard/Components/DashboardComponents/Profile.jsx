5a9e816d335ed995901f1f41b703ceee4b910993
Chibuzor <chibuzormekalam@gmail.com>
2025-02-21 12:26:09 +0100

    single user

--- src/pages/DashBoard/Components/DashboardComponents/Profile.jsx
+++ src/pages/DashBoard/Components/DashboardComponents/Profile.jsx
@@ -326,7 +326,7 @@ const Profile = ({ Handlegeneral, HandleDeleteMovie, setModalDisplay }) => {
         <div className="w-full flex justify-center">
           <div className=" md:w-1/2 w-[80%] flex flex-col justify-center items-center gap-3 p-5 rounded bg-main border border-border cursor-default">
             <div className="flex justify-center items-center">
-              {singleUser && singleUser?.image === "" ? (
+              {singleUser ? (
                 <img
                   src={`${singleUser && singleUser?.image}`}
                   alt="user"
@@ -337,7 +337,9 @@ const Profile = ({ Handlegeneral, HandleDeleteMovie, setModalDisplay }) => {
                   className="w-12 h-12 rounded-full flex items-center justify-center text-white text-lg font-bold uppercase"
                   style={{ backgroundColor: bgColor }}
                 >
-                  {singleUser ? singleUser.name.charAt(0).toUpperCase() : "?"}
+                  {singleUser && singleUser?.image === ""
+                    ? singleUser.name.charAt(0).toUpperCase()
+                    : "?"}
                 </div>
               )}
 
