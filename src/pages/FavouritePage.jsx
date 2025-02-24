ac301e09020512a08053180f76dd09a1d80ebfc4
Chibuzor <chibuzormekalam@gmail.com>
2025-02-24 10:20:51 +0100

    fixing favourite movies

--- src/pages/FavouritePage.jsx
+++ src/pages/FavouritePage.jsx
@@ -15,6 +15,10 @@ const FavouritePage = () => {
     favCartAlert,
     setFavCartAlert,
   } = useContext(MovieContext);
+  const [shareOpen, setShareOpen] = useState(false);
+  const { HandleSubscribe } = useContext(MovieContext);
+  const User = JSON.parse(localStorage.getItem("userInfo"));
+
   const GetFavouriteCart = async () => {
     try {
       const res = await fetch(
@@ -37,7 +41,6 @@ const FavouritePage = () => {
         JSON.stringify(data.data.favouriteCartMovies)
       );
       setFavouriteCartMovies(data.data.favouriteCartMovies); // Correct way to set state
-      
     } catch (error) {
       console.log("Fetch error:", error.message);
     }
@@ -65,7 +68,9 @@ const FavouritePage = () => {
   }, [FavouriteCartMovies, page]);
 
   // Total pages for pagination
-const totalPages = Math.ceil((FavouriteCartMovies?.length || 0) / itemsPerPage);
+  const totalPages = Math.ceil(
+    (FavouriteCartMovies?.length || 0) / itemsPerPage
+  );
 
   const Head = "text-xs text-left text-main font-semibold px-6 py-2 uppercase ";
   const Text = "text-sm text-left leading-6 whitespace-nowrap px-5 py-3 ";
@@ -81,6 +86,39 @@ const totalPages = Math.ceil((FavouriteCartMovies?.length || 0) / itemsPerPage);
         </div>
 
         <div className="overflow-x-scroll overflow-hidden relative w-full">
+          <div
+            className={` ${
+              shareOpen ? "" : "hidden"
+            }  absolute  top-0 left-0 z-50   w-full h-full bg-main2 flex justify-center items-center `}
+          >
+            <span
+              onClick={() => setShareOpen((prev) => !prev)}
+              className="rounded-full fixed top-[25%]  md:right-[25%] lg:right-[20%] right-[8%] border-2 h-14 w-14 border-subMain hover:text-subMain text-white transi hover:border-white hover:rotate-180 flex justify-center items-center"
+            >
+              <MdCancel className="h-12 w-12" />
+            </span>
+
+            <div className="  fixed top-[40%] flexCol gap-6  py-5 subscribe  px-4 bg-main rounded-md  justify-center  border border-border ">
+              <div className="w-1/2 flex justify-center item-center gap-2">
+                <h1 className="text-2xl font-bold">🤖Opps!</h1>
+              </div>
+              <p className="font-semibold text-text subscribe">
+                Only a valid and subscribed user can stream live. Signup or
+                login if you are already a user.
+                <span className="font-semibold text-text text-center">
+                  Please subscribe to enjoy our services.
+                </span>
+              </p>
+
+              <button
+                onClick={(e) => HandleSubscribe(e, User && User.email)}
+                className="bg-subMain2 text-white rounded-md border-2 border-subMain transi mt-3 hover:bg-main p-2 animate-bounce hover:animate-none "
+              >
+                {User ? "Subscribe" : "Login"}
+              </button>
+            </div>
+          </div>
+
           <table className="table-auto w-full text-white border  border-border divide-y divide-border">
             <thead>
               <tr className="bg-dryGray">
@@ -106,7 +144,6 @@ const totalPages = Math.ceil((FavouriteCartMovies?.length || 0) / itemsPerPage);
               </tr>
             </thead>
             <tbody className=" bg-main divide-y divide-gray-800">
-              
               {paginatedMovies.map((movie, i) => (
                 <tr key={i}>
                   <td className={`${Text}`}>
@@ -135,7 +172,19 @@ const totalPages = Math.ceil((FavouriteCartMovies?.length || 0) / itemsPerPage);
                       Download <FaCloudDownloadAlt />
                     </button>
                     <Link
-                      to={`/watch/${movie.movie?.id}`}
+                      onClick={(e) => {
+                        if (User.subscription !== "SUBSCRIBED") {
+                          setShareOpen((prev) => !prev);
+                        } else {
+                          navigate(`/stream/watch/${movie?.name}`);
+                          // console.log("User already Subscribed!");
+                        }
+                      }}
+                      to={`${
+                        User && User.subscription === "SUBSCRIBED"
+                          ? `/watch/${movie?.name}`
+                          : ``
+                      }`}
                       className="bg-subMain text-white  hover:bg-main transi border border-subMain rounded flexCol w-6 h-6 "
                     >
                       <GoEye />
