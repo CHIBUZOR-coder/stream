d27dcb6d101194153ba6741233dddd6a1dcbdae5
Chibuzor <chibuzormekalam@gmail.com>
2025-02-26 08:05:37 +0100

    adding HandleGtWtchCount calls7

--- src/pages/Watch.jsx
+++ src/pages/Watch.jsx
@@ -90,9 +90,9 @@ const Watch = () => {
           <div className="w-full md:w-1/3 flex justify-between p-2 gap-5 ">
             <Link
               to={`/movie/${movie?.id}`}
-              className="btn md:text-xl  text-xl flex gap-3 items-center font-bold text-dryGray"
+              className="btn md:text-xl  flex gap-3 items-center  text-dryGray"
             >
-              <BiArrowBack />
+              <BiArrowBack className="text-xl font-bold " />
             </Link>
             <p className="btn md:text-xl text-sm flex gap-3 items-center font-bold text-dryGray">
               {movie && movie?.name}
