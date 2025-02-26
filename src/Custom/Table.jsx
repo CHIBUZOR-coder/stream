a6a65aa1d2990bfbe5387642938175cf67458f81
Chibuzor <chibuzormekalam@gmail.com>
2025-02-26 10:17:26 +0100

    fixing display in profile17

--- src/Custom/Table.jsx
+++ src/Custom/Table.jsx
@@ -64,130 +64,152 @@ const Table = ({
           </tr>
         </thead>
         <tbody className=" bg-main divide-y divide-gray-800">
-          {data &&
-            data.map((movie, i) => (
-              <tr key={i}>
-                {/* {console.log(movie)} */}
-                <td className={`${Text}`}>
-                  <div className="w-12 bg-dry borer border-border rounded h-12 overflow-hidden ">
-                    <img
-                      src={`${
-                        For === "movie" ? movie?.image : movie?.movie?.image
-                      }`}
-                      alt={For === "movie" ? movie.image : movie.movie?.image}
-                      className="w-full h-full object-cover rounded-md"
-                    />
-                  </div>
-                </td>
-                <td className={`${Text}`}>
-                  <p>{For === "movie" ? movie.name : movie.movie?.name}</p>
-                </td>
-                <td className={`${Text}`}>
-                  <p>
-                    {For === "movie"
-                      ? movie?.category?.tittle
-                      : movie?.movie?.category?.tittle}
-                  </p>
-                </td>
+          {For === "movie" ? (
+            <>
+              {" "}
+              {data &&
+                data.map((movie, i) => (
+                  <tr key={i}>
+                    {/* {console.log(movie)} */}
+                    <td className={`${Text}`}>
+                      <div className="w-12 bg-dry borer border-border rounded h-12 overflow-hidden ">
+                        <img
+                          src={`${movie?.image}`}
+                          alt={movie?.image}
+                          className="w-full h-full object-cover rounded-md"
+                        />
+                      </div>
+                    </td>
+                    <td className={`${Text}`}>
+                      <p>{movie?.name}</p>
+                    </td>
+                    <td className={`${Text}`}>
+                      <p>{movie?.category?.tittle}</p>
+                    </td>
 
-                <td className={`${Text}`}>
-                  <p>{For === "movie" ? movie?.year : movie?.movie?.year}</p>
-                </td>
-                <td className={`${Text}`}>
-                  <p> {For === "movie" ? movie?.time : movie?.movie?.time}</p>
-                </td>
-                <td className={`${Text} float-right flexRow gap-2`}>
-                  {For === "dash" ? (
-                    <>
-                      {" "}
-                      <button className="bg-dry border border-border flexRow gap-2 text-border px-2 py-1 rounded">
-                        Download <FaCloudDownloadAlt />
-                      </button>
-                      <Link
-                        onClick={(e) => {
-                          e.preventDefault();
-                          if (User.subscription !== "SUBSCRIBED") {
-                            setShareOpen((prev) => !prev);
-                          } else {
-                            navigate(`/stream/watch/${movie?.name}`);
-                            // console.log("User already Subscribed!");
-                          }
-                        }}
-                        to={`${
-                          User && User.subscription === "SUBSCRIBED"
-                            ? `/watch/${movie?.name}`
-                            : ``
-                        }`}
-                        className="bg-subMain text-white  hover:bg-main transi border border-subMain rounded flexCol w-6 h-6 "
-                      >
-                        <GoEye />
-                      </Link>
-                    </>
-                  ) : For === "movie" ? (
-                    <>
-                      {/* <button className="bg-dry border border-border flexRow gap-2 text-border px-2 py-1 rounded">
-                        Download <FaCloudDownloadAlt />
-                      </button> */}
+                    <td className={`${Text}`}>
+                      <p>{movie?.year}</p>
+                    </td>
+                    <td className={`${Text}`}>
+                      <p> {movie?.time}</p>
+                    </td>
+                    <td className={`${Text} float-right flexRow gap-2`}>
+                      {For === "dash" ? (
+                        <>
+                          {" "}
+                          <button className="bg-dry border border-border flexRow gap-2 text-border px-2 py-1 rounded">
+                            Download <FaCloudDownloadAlt />
+                          </button>
+                          <Link
+                            onClick={(e) => {
+                              e.preventDefault();
+                              if (User.subscription !== "SUBSCRIBED") {
+                                setShareOpen((prev) => !prev);
+                              } else {
+                                navigate(`/stream/watch/${movie?.name}`);
+                                // console.log("User already Subscribed!");
+                              }
+                            }}
+                            to={`${
+                              User && User.subscription === "SUBSCRIBED"
+                                ? `/watch/${movie?.name}`
+                                : ``
+                            }`}
+                            className="bg-subMain text-white  hover:bg-main transi border border-subMain rounded flexCol w-6 h-6 "
+                          >
+                            <GoEye />
+                          </Link>
+                        </>
+                      ) : For === "movie" ? (
+                        <>
+                          <button
+                            onClick={() => Handlegeneral(movie.name, movie.id)}
+                            className="bg-dry border border-border flexRow gap-2 text-border transi edit  hover:bg-green-500 hover:text-white px-2 py-1 rounded"
+                          >
+                            Edit{" "}
+                            <FaEdit className="text-green-500 editchild  transi " />
+                          </button>
 
-                      <button
-                        onClick={() => Handlegeneral(movie.name, movie.id)}
-                        className="bg-dry border border-border flexRow gap-2 text-border transi edit  hover:bg-green-500 hover:text-white px-2 py-1 rounded"
-                      >
-                        Edit{" "}
-                        <FaEdit className="text-green-500 editchild  transi " />
-                      </button>
+                          <button
+                            onClick={(e) => {
+                              HandleDeleteMovie(e, movie.id);
+                            }}
+                            className="bg-subMain text-white rounded flexCol w-6 h-6  hover:bg-main transi border border-subMain delete  "
+                          >
+                            <MdDelete className="deletechild transi" />
+                          </button>
+                        </>
+                      ) : (
+                        ""
+                      )}
+                    </td>
+                  </tr>
+                ))}
+            </>
+          ) : (
+            <>
+              {" "}
+              {data &&
+                data.map((movie, i) => (
+                  <tr key={i}>
+                    {/* {console.log(movie)} */}
+                    <td className={`${Text}`}>
+                      <div className="w-12 bg-dry borer border-border rounded h-12 overflow-hidden ">
+                        <img
+                          src={`${movie?.movie?.image}`}
+                          alt={movie.movie?.image}
+                          className="w-full h-full object-cover rounded-md"
+                        />
+                      </div>
+                    </td>
+                    <td className={`${Text}`}>
+                      <p>{movie.movie?.name}</p>
+                    </td>
+                    <td className={`${Text}`}>
+                      <p>{movie?.movie?.category?.tittle}</p>
+                    </td>
 
-                      <button
-                        onClick={(e) => {
-                          HandleDeleteMovie(e, movie.id);
-                        }}
-                        className="bg-subMain text-white rounded flexCol w-6 h-6  hover:bg-main transi border border-subMain delete  "
-                      >
-                        <MdDelete className="deletechild transi" />
-                      </button>
-                    </>
-                  ) : (
-                    ""
-                  )}
-
-                  {/* <button className="bg-dry border border-border flexRow gap-2 text-border px-2 py-1 rounded">
-                    Download <FaCloudDownloadAlt />
-                  </button>
-
-                  <button
-                    onClick={() => Handlegeneral(movie.name, movie.id)}
-                    className="bg-dry border border-border flexRow gap-2 text-border transi edit  hover:bg-green-500 hover:text-white px-2 py-1 rounded"
-                  >
-                    Edit{" "}
-                    <FaEdit className="text-green-500 editchild  transi " />
-                  </button>
-
-                  <button
-                    onClick={(e) => {
-                      HandleDeleteMovie(e, movie.id);
-                    }}
-                    className="bg-subMain text-white rounded flexCol w-6 h-6  hover:bg-main transi border border-subMain delete  "
-                  >
-                    <MdDelete className="deletechild transi" />
-                  </button>
- */}
-                  {/* <>
-                      <button className="bg-dry border border-border flexRow gap-2 text-border px-2 py-1 rounded">
-                        Download <FaCloudDownloadAlt />
-                      </button>
-                      <Link
-                        to={`/stream/watch/${
-                          For === "movie" ? `${movie.id}` : `${movie.movie?.id}`
-                        }`}
-                        className="bg-subMain text-white rounded flexCol w-6 h-6 "
-                      >
-                        <GoEye />
-                      </Link>
-                    </>
-                   */}
-                </td>
-              </tr>
-            ))}
+                    <td className={`${Text}`}>
+                      <p>{movie?.movie?.year}</p>
+                    </td>
+                    <td className={`${Text}`}>
+                      <p> {movie?.movie?.time}</p>
+                    </td>
+                    <td className={`${Text} float-right flexRow gap-2`}>
+                      {For === "dash" ? (
+                        <>
+                          {" "}
+                          <button className="bg-dry border border-border flexRow gap-2 text-border px-2 py-1 rounded">
+                            Download <FaCloudDownloadAlt />
+                          </button>
+                          <Link
+                            onClick={(e) => {
+                              e.preventDefault();
+                              if (User.subscription !== "SUBSCRIBED") {
+                                setShareOpen((prev) => !prev);
+                              } else {
+                                navigate(`/stream/watch/${movie?.movie?.name}`);
+                                // console.log("User already Subscribed!");
+                              }
+                            }}
+                            to={`${
+                              User && User.subscription === "SUBSCRIBED"
+                                ? `/watch/${movie?.movie?.name}`
+                                : ``
+                            }`}
+                            className="bg-subMain text-white  hover:bg-main transi border border-subMain rounded flexCol w-6 h-6 "
+                          >
+                            <GoEye />
+                          </Link>
+                        </>
+                      ) : (
+                        ""
+                      )}
+                    </td>
+                  </tr>
+                ))}
+            </>
+          )}
         </tbody>
       </table>
     </div>
