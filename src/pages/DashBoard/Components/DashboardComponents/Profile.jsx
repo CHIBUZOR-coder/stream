9a979fbd7f6d9249c08c52f1a36e6b7e343ccf36
Chibuzor <chibuzormekalam@gmail.com>
2025-02-26 08:31:11 +0100

    fixing display in profile

--- src/pages/DashBoard/Components/DashboardComponents/Profile.jsx
+++ src/pages/DashBoard/Components/DashboardComponents/Profile.jsx
@@ -68,6 +68,12 @@ const Profile = ({ Handlegeneral, HandleDeleteMovie, setModalDisplay }) => {
   }
   // console.log("UserLink", UserLink);
 
+
+   useEffect(() => {
+     console.log("watched:", watched);
+     console.log("watchedLength:", watched.length);
+   }, [watched]);
+
   // Paginated movies for the current page
   const paginatedMovies = useMemo(() => {
     return (AllMovies || [])
