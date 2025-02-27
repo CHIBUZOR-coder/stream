46f82da5685ddea00b51ef0439dd80579d2950da
Chibuzor <chibuzormekalam@gmail.com>
2025-02-27 05:56:06 +0100

    changed type to label

--- src/Custom/Input.jsx
+++ src/Custom/Input.jsx
@@ -31,7 +31,7 @@ export const Input = ({
         required={false}
       />
 
-      {type === "Password" ? (
+      {label === "Password" ? (
         <div
           onClick={() => setVisible((prev) => !prev)}
           className="absolute right-4 top-[60%] cursor-pointer"
