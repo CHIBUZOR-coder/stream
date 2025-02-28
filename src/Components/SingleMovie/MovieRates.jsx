f53648dcc1fe95d06628fdfa940ea43f8015d781
Chibuzor <chibuzormekalam@gmail.com>
2025-02-28 10:06:09 +0100

    user review images5

--- src/Components/SingleMovie/MovieRates.jsx
+++ src/Components/SingleMovie/MovieRates.jsx
@@ -40,8 +40,6 @@ const MovieRates = ({ movie }) => {
   //   console.log("reviews:", reviews);
   // }, [reviews]);
 
-
-
   const HandleReview = async (e) => {
     e.preventDefault();
     if (!User) {
@@ -122,6 +120,15 @@ const MovieRates = ({ movie }) => {
       value: 5,
     },
   ];
+
+   const getRandomColor = (seed) => {
+     let hash = 0;
+     for (let i = 0; i < seed.length; i++) {
+       hash = seed.charCodeAt(i) + hash * 31; // Replacing (hash << 5) - hash with hash * 31
+     }
+     const color = `#${(hash & 0xffffff).toString(16).padStart(6, "0")}`; // Ensure 6-digit hex
+     return color;
+   };
   return (
     <div className="my-12">
       <div className="flex items-center gap-8 md:gap-4">
@@ -226,7 +233,14 @@ const MovieRates = ({ movie }) => {
                       key={i}
                       className="md:grid   flex flex-col w-full grid-cols-12 gap-5  bg-dry p-4 border border-x-gray-800  rounded-lg"
                     >
-                      <div className="col-span-2 ">
+                      <div
+                        style={{
+                          backgroundColor: starUsers?.image
+                            ? "transparent"
+                            : getRandomColor(starUsers?.name || starUsers?.id),
+                        }}
+                        className="col-span-2"
+                      >
                         <img
                           className="w-full  object-cover"
                           src={`${user?.user?.image}`}
