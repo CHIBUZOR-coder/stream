b755c3f1721edf2822175ef1cff9723544afe731
Chibuzor <chibuzormekalam@gmail.com>
2025-02-28 10:02:31 +0100

    user review images4

--- src/Components/SingleMovie/MovieRates.jsx
+++ src/Components/SingleMovie/MovieRates.jsx
@@ -122,15 +122,6 @@ const MovieRates = ({ movie }) => {
       value: 5,
     },
   ];
-
-  const getRandomColor = (seed) => {
-    let hash = 0;
-    for (let i = 0; i < seed.length; i++) {
-      hash = seed.charCodeAt(i) + hash * 31; // Replacing (hash << 5) - hash with hash * 31
-    }
-    const color = `#${(hash & 0xffffff).toString(16).padStart(6, "0")}`; // Ensure 6-digit hex
-    return color;
-  };
   return (
     <div className="my-12">
       <div className="flex items-center gap-8 md:gap-4">
@@ -223,9 +214,7 @@ const MovieRates = ({ movie }) => {
 
         {/* Review */}
         <div className="col-span-3 flex flex-col gap-6 w-full md:w-auto p-4 md:p-0">
-          <p className="text-xl text-text font-semibold">
-            Reviews {(starUsers && starUsers?.length) || 0}
-          </p>
+          <p className="text-xl text-text font-semibold">Reviews (56)</p>
           <div
             className={`w-full flex flex-col bg-main gap-6 rounded-lg md:p-12 p-0   max-h-[460px] md:max-h-[550px] overflow-y-scroll `}
           >
@@ -237,16 +226,7 @@ const MovieRates = ({ movie }) => {
                       key={i}
                       className="md:grid   flex flex-col w-full grid-cols-12 gap-5  bg-dry p-4 border border-x-gray-800  rounded-lg"
                     >
-                      <div
-                        style={{
-                          backgroundColor: starUsers?.image
-                            ? "transparent"
-                            : getRandomColor(
-                                starUsers?.name || starUsers?.id
-                              ),
-                        }}
-                        className="col-span-2 "
-                      >
+                      <div className="col-span-2 ">
                         <img
                           className="w-full  object-cover"
                           src={`${user?.user?.image}`}
